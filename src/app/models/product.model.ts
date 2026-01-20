7b83016d8d3ab780770d342d7fb05c108c6b07f3
Radu-A <elcastravete@gmail.com>
2026-01-20 19:34:37 +0100

    feat(product.model): add ProductModel interface

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,10 @@
+export interface ProductModel {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  stock: number;
+  thumbnail: string;
+  images: string[];
+}
