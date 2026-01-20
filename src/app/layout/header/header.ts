2a6184f45df1246d5aac048ea407a78fd8c9bda9
Radu-A <elcastravete@gmail.com>
2026-01-20 14:15:08 +0100

    feat(header): add component, style and links

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
