83e3a55bedb920b2d478ee471ba9c270708bf85a
Radu-A <elcastravete@gmail.com>
2026-01-20 11:30:59 +0100

    chore: create file structure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { MainLayout } from './layout/main-layout/main-layout';
+import { ProductList } from './pages/products/product-list/product-list';
+import { ProductDetails } from './pages/products/product-details/product-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'products',
+        children: [
+          {
+            path: '',
+            component: ProductList,
+          },
+          {
+            path: 'details',
+            component: ProductDetails,
+          },
+        ],
+      },
+    ],
+  },
+];
