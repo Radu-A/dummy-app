283e540a6d04cb83c931666f6044e7506e7ce0e8
Radu-A <elcastravete@gmail.com>
2026-01-20 19:20:05 +0100

    feat(footer): add component, styles and legal links

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink, MatToolbarModule, MatButtonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {
-
-}
+export class Footer {}
