15515de352b1ef10ef25c40098ab9e3ce648383b
Radu-A <elcastravete@gmail.com>
2026-01-21 13:54:47 +0100

    feat(product-list): add search input and product grid to product-list component

--- src/app/pages/products/product-list/product-list.ts
+++ src/app/pages/products/product-list/product-list.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { ProductModel } from '../../../models/product.model';
+import { ProductService } from '../../../services/product-service';
+
+import { ProductCard } from '../../../components/product-card/product-card';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [AsyncPipe, MatFormFieldModule, MatInputModule, MatGridListModule, ProductCard],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
 export class ProductList {
+  service = inject(ProductService);
 
+  listState$: Observable<ProductModel[]> = this.service.getProducts();
 }
