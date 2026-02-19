d11560c4665ad9be0397e12830ddcfe105227f91
Radu-A <elcastravete@gmail.com>
2026-02-19 13:35:13 +0100

    feat(test-message): create component and add to login

---
+++ src/app/components/test-message/test-message.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-message',
+  imports: [],
+  templateUrl: './test-message.html',
+  styleUrl: './test-message.scss',
+})
+export class TestMessage {}
